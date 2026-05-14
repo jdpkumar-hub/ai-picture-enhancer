3cef1beef9e6265bce7e114e03438535a4387ad2
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-05-14 00:01:06 -0500

    Add files via upload

--- auth.py
+++ auth.py
@@ -6,14 +6,25 @@ supabase = create_client(
     st.secrets["SUPABASE_KEY"]
 )
 
+# ---------- LOGIN ----------
 def login(email, password):
     return supabase.auth.sign_in_with_password({
         "email": email,
         "password": password
     })
 
+# ---------- SIGNUP ----------
 def signup(email, password):
     return supabase.auth.sign_up({
         "email": email,
         "password": password
     })
+
+# ---------- RESET PASSWORD ----------
+def reset_password(email):
+    return supabase.auth.reset_password_email(
+        email,
+        {
+            "redirect_to": "https://ai-enhancer.streamlit.app"
+        }
+    )
