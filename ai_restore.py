378b0fddd3f796d5615cc7fc767a1fa69e60787b
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-05-14 13:50:38 -0500

    Add files via upload

--- ai_restore.py
+++ ai_restore.py
@@ -2,28 +2,31 @@ import replicate
 import streamlit as st
 import tempfile
 
-# Initialize Replicate client
+# =========================================
+# REPLICATE CLIENT
+# =========================================
 client = replicate.Client(
     api_token=st.secrets["REPLICATE_API_TOKEN"]
 )
 
 # =========================================
-# AI FACE RESTORATION
+# AI FACE RESTORE
 # =========================================
 def restore_face(uploaded_file):
 
-    # Save uploaded image temporarily
+    # Save uploaded image
     with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp:
 
         temp.write(uploaded_file.read())
 
         temp_path = temp.name
 
-    # Run GFPGAN model
+    # Run model
     output = client.run(
         "nightmareai/real-esrgan",
         input={
             "image": open(temp_path, "rb"),
+            "scale": 2,
             "face_enhance": True
         }
     )
