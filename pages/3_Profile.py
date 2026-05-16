fe045bd3f9ba31ad3855fff152d1893fdb9be10c
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-05-15 19:51:57 -0500

    Add files via upload

--- pages/3_Profile.py
+++ pages/3_Profile.py
@@ -1,1 +1,36 @@
-3_Profile.py
+import streamlit as st
+from auth import logout
+
+# =====================================================
+# AUTH CHECK
+# =====================================================
+
+if "user" not in st.session_state:
+
+    st.switch_page("app.py")
+
+# =====================================================
+# SIDEBAR
+# =====================================================
+
+st.sidebar.success(
+    f"Welcome {st.session_state.user.email}"
+)
+
+if st.sidebar.button("Logout"):
+
+    logout()
+
+    st.session_state.user = None
+
+    st.switch_page("app.py")
+
+# =====================================================
+# PAGE
+# =====================================================
+
+st.title("👤 Profile")
+
+st.write(
+    f"Logged in as: {st.session_state.user.email}"
+)
