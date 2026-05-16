14a1cdadb053c729548172425acd7c9b6ee74ca4
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-05-16 18:17:13 -0500

    Add files via upload

--- pages/1_Enhance.py
+++ pages/1_Enhance.py
@@ -52,40 +52,81 @@ if st.session_state.user is None:
 # SIDEBAR
 # =====================================================
 
+# =====================================================
+# SIDEBAR
+# =====================================================
+
 with st.sidebar:
 
     st.image(
         "assets/logo.png",
-        width=270
+        width=240
     )
 
-    st.caption(" Smart image Enhancer")
+    st.caption("🚀 Smart image Enhancer")
 
     st.markdown(
         "<div class='sidebar-divider'></div>",
         unsafe_allow_html=True
     )
 
-    if st.button("🏠 Home",use_container_width=True):
+    # ==========================================
+    # MENU
+    # ==========================================
+
+    if st.button(
+        "🏠 Home",
+        use_container_width=True
+    ):
         st.switch_page("app.py")
 
-    if st.button("📜 History",use_container_width=True):
+    if st.button(
+        "✨ Enhance",
+        use_container_width=True
+    ):
+        st.switch_page("pages/1_Enhance.py")
+
+    if st.button(
+        "📜 History",
+        use_container_width=True
+    ):
         st.switch_page("pages/2_History.py")
 
-    if st.button("👤 Profile",use_container_width=True):
+    if st.button(
+        "👤 Profile",
+        use_container_width=True
+    ):
         st.switch_page("pages/3_Profile.py")
-    st.markdown("<div class='sidebar-divider'></div>",unsafe_allow_html=True)
-    
-    if st.sidebar.button("🚪 Logout",use_container_width=True):
 
-    logout()
-    st.session_state.user = None
-    st.switch_page("app.py")
-    st.stop()
-    
-st.sidebar.success(
-    f"Welcome {st.session_state.user.email}"
-)
+    st.markdown(
+        "<div class='sidebar-divider'></div>",
+        unsafe_allow_html=True
+    )
+
+    # ==========================================
+    # LOGOUT
+    # ==========================================
+
+    if st.button(
+        "🚪 Logout",
+        use_container_width=True
+    ):
+
+        logout()
+
+        st.session_state.user = None
+
+        st.switch_page("app.py")
+
+        st.stop()
+
+    # ==========================================
+    # USER
+    # ==========================================
+
+    st.success(
+        f"Welcome {st.session_state.user.email}"
+    )
 
 # =====================================================
 # REPLICATE TOKEN
