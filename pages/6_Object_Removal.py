4922dc3152421cbd967623a487263785ae369f2b
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-05-16 21:27:49 -0500

    Add files via upload

--- pages/6_Object_Removal.py
+++ pages/6_Object_Removal.py
@@ -10,6 +10,40 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# =====================================
+# SIDEBAR
+# =====================================
+
+with st.sidebar:
+
+    st.image(
+        "assets/logo.png",
+        width=240
+    )
+
+    st.caption("🚀 Smart image Enhancer")
+
+    st.markdown(
+        "<div class='sidebar-divider'></div>",
+        unsafe_allow_html=True
+    )
+
+    if st.button("🏠 Home", use_container_width=True):
+        st.switch_page("app.py")
+
+    if st.button("✨ Enhance", use_container_width=True):
+        st.switch_page("pages/1_Enhance.py")
+
+    if st.button("🪄 Remove Background", use_container_width=True):
+        st.switch_page("pages/4_Background_Remove.py")
+
+    if st.button("🧓 Restore Photo", use_container_width=True):
+        st.switch_page("pages/5_Old_Photo_Restore.py")
+
+    if st.button("🧼 Remove Objects", use_container_width=True):
+        st.switch_page("pages/6_Object_Removal.py")
+
+
 st.title("🧼 Object Removal")
 
 uploaded = st.file_uploader(
