9cc4d0e246241a111adb7dd60625953a3b2d9b4b
Pradarshan Jupidi <jdpkumar@gmail.com>
2026-05-16 20:23:34 -0500

    Add files via upload

--- pages/3_Profile.py
+++ pages/3_Profile.py
@@ -103,17 +103,6 @@ with st.sidebar:
 # HEADER
 # =====================================================
 
-col1, col2 = st.columns([1, 5])
-
-with col1:
-
-    st.image(
-        "assets/logo.png",
-        width=80
-    )
-
-with col2:
-
     st.title("👤 Profile")
 
 # =====================================================
